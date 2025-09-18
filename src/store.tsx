24f0a0d39f5a28eb9abff463b50594244e9a60aa
Juan Cantero <juan.cantero.jarilla@gmail.com>
2025-09-18 12:23:54 +0200

    adaptaciones para futura API con Django

--- src/store.tsx
+++ src/store.tsx
@@ -1,21 +1,48 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+
+interface WeatherData {
+  current: {
+    temp: number;
+    wind_speed: number;
+    dt: number;
+    weather?: { main: string; description: string }[];
+  };
+  hourly?: Array<{
+    dt: number;
+    temp: number;
+    weather: { main: string }[];
+  }>;
+  lat?: number;
+  lon?: number;
+}
+
+interface ForecastData {
+  [period: string]: any;
+}
+
 interface WeatherState {
   city: string;
-  weatherData: any;
-  forecastData: any;
+  weatherData: WeatherData | null;
+  forecastData: ForecastData;
   setCity: (city: string) => void;
-  setWeatherData: (data: any) => void;
-  setForecastData: (data: any) => void;
+  setWeatherData: (data: WeatherData) => void;
+  setForecastData: (period: string, data: any) => void;
 }
 
 export const useWeatherStore = create<WeatherState>()(
   devtools((set) => ({
     city: "-",
     weatherData: null,
-    forecastData: null,
+    forecastData: {},
     setCity: (city) => set({ city }),
     setWeatherData: (data) => set({ weatherData: data }),
-    setForecastData: (data) => set({ forecastData: data }),
+    setForecastData: (period, data) =>
+      set((state) => ({
+        forecastData: {
+          ...state.forecastData,
+          [period]: data,
+        },
+      })),
   }))
 );
